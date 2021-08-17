698225a1bc0131056fb1194be655dd2d708373f5
Daniel Hausner <dn.hausner@gmail.com>
2021-08-17 15:30:27 +0200

    feat(CodeQuality): set up Prettier & Husky

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'components', 'lib'], // Only run ESLint on the these dirs
+  },
 }
