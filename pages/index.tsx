bccd0f8cff6d5d76513a82189cf9396c0cae140c
Daniel Hausner <dn.hausner@gmail.com>
2021-08-17 18:30:08 +0200

    imp(Home): add name and one sentence intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import {Box, Text} from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
-    <Box bg='#F7EBD4' h='100vh'>
+    <Box bg="rgba(247, 235, 212, 1)" h="100vh" >
       <Head>
-        <title>I{'\''}m Daniel Hausner</title>
+        <title>I{"'"}m Daniel Hausner</title>
         <meta name="description" content="I'm Daniel Hausner, fullstack web developer." />
         {/*<link rel="icon" href="/favicon.ico" />*/}
       </Head>
-      <Text>Hello</Text>
+
+      <Box py={20}>
+        <Heading fontSize="6xl" lineHeight="9">Daniel</Heading>
+        <Heading fontSize="2.35rem">HAUSNER</Heading>
+        <Heading as={'h1'} fontWeight="medium" fontSize="xl">I bring ideas to live with web technologies.</Heading>
+      </Box>
+
       <footer>Placeholder for footer.</footer>
     </Box>
   );
