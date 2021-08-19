1a08b8f2b0bfeb61339cf20aae3ea219df0d26bc
Daniel Hausner <dn.hausner@gmail.com>
2021-08-19 15:22:52 +0200

    refactor(DatoCMS): extract responsiveImageFragment

--- lib/datocms.ts
+++ lib/datocms.ts
@@ -1,5 +1,22 @@
 import { GraphQLClient } from 'graphql-request';
-import {RequestDocument, Variables} from "graphql-request/dist/types";
+import { RequestDocument, Variables } from 'graphql-request/dist/types';
+
+// See: https://www.datocms.com/blog/offer-responsive-progressive-lqip-images-in-2020
+const responsiveImageFragment = `
+  fragment responsiveImageFragment on ResponsiveImage {
+    srcSet
+    webpSrcSet
+    sizes
+    src
+    width
+    height
+    aspectRatio
+    alt
+    title
+    bgColor
+    base64
+  }
+`;
 
 interface RequestParams {
   query: RequestDocument;
@@ -8,7 +25,10 @@ interface RequestParams {
 }
 
 export function request({ query, variables, preview }: RequestParams) {
-  const endpoint = preview ? `https://graphql.datocms.com/preview` : `https://graphql.datocms.com/`;
+  const endpoint = preview
+    ? `
+https://graphql.datocms.com/preview`
+    : `https://graphql.datocms.com/`;
   const client = new GraphQLClient(endpoint, {
     headers: {
       authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`,
@@ -21,24 +41,15 @@ const HOMEPAGE_QUERY = `query HomePage {
   home {
     profilePicture {
       responsiveImage(imgixParams: { fit: crop, w: 100, h: 100, auto: format }) {
-        srcSet
-        webpSrcSet
-        sizes
-        src
-        width
-        height
-        aspectRatio
-        alt
-        title
-        base64
+        ...responsiveImageFragment
       }
     }
   }
 }`;
 
-export const getHomeData = async() => {
+export const getHomeData = async () => {
   const data = await request({
     query: HOMEPAGE_QUERY,
   });
   return data?.home;
-}
+};
