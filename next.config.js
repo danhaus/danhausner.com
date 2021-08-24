137076d318062be5d03e32641c274af73084d54b
Daniel Hausner <dn.hausner@gmail.com>
2021-08-24 20:24:19 +0200

    refactor(Project): move components, libs, and pages to src dir

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['www.datocms-assets.com']
+    domains: ['www.datocms-assets.com'],
   },
   eslint: {
-    dirs: ['pages', 'components', 'lib'], // Only run ESLint on the these dirs
+    dirs: ['src'], // Only run ESLint on the these dirs
   },
-}
+};
