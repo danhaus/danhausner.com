dd8f4f9dd00ff5d8bf404d89bb1ef5124b4c289c
Daniel Hausner <dn.hausner@gmail.com>
2021-08-26 18:53:19 +0200

    refactor(Home): extract Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@chakra-ui/react';
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <Box bg="rgba(247, 235, 212, 1)">
+      {children}
+      <footer>Placeholder for footer.</footer>
+    </Box>
+  );
+};
+
+export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
-import { Box, Text } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 import { getHomeData } from '../lib/datocms';
 import { ResponsiveImageType } from 'react-datocms';
 import Hero from '../components/Hero';
@@ -24,7 +24,7 @@ const Home: NextPage<HomeProps> = ({ homeData }) => {
   // console.log(JSON.stringify(home));
   const { profilePicture } = homeData;
   return (
-    <Box bg="rgba(247, 235, 212, 1)">
+    <>
       <Head>
         <title>I{"'"}m Daniel Hausner</title>
         <meta name="description" content="I'm Daniel Hausner, fullstack web developer." />
@@ -49,9 +49,7 @@ const Home: NextPage<HomeProps> = ({ homeData }) => {
         and climbing. I also do calisthenics and run regularly — currently training for a marathon.
       </Text>
       <br />
-
-      <footer>Placeholder for footer.</footer>
-    </Box>
+    </>
   );
 };
 
