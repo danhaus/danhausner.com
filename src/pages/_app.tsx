597bb4641f344732db8091029e3f76de779e86b2
Daniel Hausner <dn.hausner@gmail.com>
2021-08-26 19:49:56 +0200

    imp(Theme): set body color by extending Chakra theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
+import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'rgba(247, 235, 212, 1)',
+      },
+    },
+  },
+});
+
+export default theme;
