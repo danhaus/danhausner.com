5f99ceff2eca0ce89617ddacc1da6d560deac49e
Daniel Hausner <dn.hausner@gmail.com>
2021-09-06 18:12:21 +0200

    imp(Navigation): create hidden offset anchor in Section.tsx and use the default behaviour to scroll to anchor instead of the useEffect in _app.tsx

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,12 +1,25 @@
 import { Box, BoxProps, Heading } from '@chakra-ui/react';
+import { NAV_BAR_HEIGHT } from '../navigation/constants';
 
 interface SectionProps extends BoxProps {
   heading?: string;
 }
 
-const Section = ({ heading, children, ...rest }: SectionProps) => {
+const Section = ({ heading, children, id, ...rest }: SectionProps) => {
   return (
     <Box as="section" {...rest}>
+      {/* Offset anchor by navbar height for automatic scrolling to section */}
+      {id && (
+        <a
+          id={id}
+          style={{
+            display: 'block',
+            position: 'relative',
+            top: `-${NAV_BAR_HEIGHT}px`,
+            visibility: 'hidden',
+          }}
+        />
+      )}
       {heading && <Heading mb={4}>{heading}</Heading>}
       {children}
     </Box>
