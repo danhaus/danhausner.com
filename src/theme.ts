1e6485c5f7d8e9d662f98c89701c00b0a91a2917
Daniel Hausner <dn.hausner@gmail.com>
2021-09-08 17:32:11 +0200

    imp(Theme): set global body color and add secondary and tertiary colors to theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -2,14 +2,27 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   colors: {
+    // Backgrounds
     primary: {
       light: '#f7ebd4',
+      dark: '#141b1f',
+    },
+    // Font
+    secondary: {
+      light: '#292929',
+      dark: '#f5f5f5',
+    },
+    // Dimmed font
+    tertiary: {
+      light: '#595959',
+      dark: '#b3b3b3',
     },
   },
   styles: {
     global: {
       body: {
         bg: 'primary.light',
+        color: 'secondary.light',
       },
     },
   },
