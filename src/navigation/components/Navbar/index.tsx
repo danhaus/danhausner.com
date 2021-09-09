e63de329bb4b491663fc094ee968b776d59e4415
Daniel Hausner <dn.hausner@gmail.com>
2021-09-09 12:02:07 +0200

    imp(Navigation): remove logo from Navbar

--- src/navigation/components/Navbar/index.tsx
+++ src/navigation/components/Navbar/index.tsx
@@ -8,8 +8,6 @@ import {
   DrawerOverlay,
   Flex,
   IconButton,
-  Text,
-  useBreakpointValue,
   useColorModeValue,
   useDisclosure,
 } from '@chakra-ui/react';
@@ -67,14 +65,6 @@ const Navbar = () => {
           />
         </Flex>
         <Flex display={{ base: 'none', md: 'flex' }} flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          <Text
-            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-            fontFamily={'heading'}
-            color={useColorModeValue('gray.800', 'white')}
-          >
-            Logo
-          </Text>
-
           <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
             <DesktopNav visibleSection={visibleSectionId} />
           </Flex>
